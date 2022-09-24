88d4500783fcce259f6fef2b38b9d24ec59f000e
Gonzalo Alvarez <gonza2703@gmail.com>
2022-09-24 00:49:18 -0300

    Added home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [ {
+  path: 'experience',
+  component: ExperienceComponent
+}, {
+  path: 'projects',
+  component: ProjectsComponent
+}, {
+  path: 'home',
+  component: HomeComponent
+}, { 
+  path: '',
+  redirectTo: '/home', 
+  pathMatch: 'full'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
