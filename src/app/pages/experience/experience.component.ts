2240df30198644f4acdf8c3b2eb795b92d180cdf
Gonzalo Alvarez <gonza2703@gmail.com>
2022-09-25 19:47:31 -0300

    Added projects page

--- src/app/pages/experience/experience.component.ts
+++ src/app/pages/experience/experience.component.ts
@@ -10,6 +10,7 @@ export class ExperienceComponent implements OnInit {
   experiences = [
     {
       title: 'Backend Developer',
+      url: 'https://www.uala.com.ar/',
       logo: 'https://seeklogo.com/images/U/uala-logo-7959775EA9-seeklogo.com.png',
       employer: 'Uala\u0301',
       to: undefined,
@@ -18,6 +19,7 @@ export class ExperienceComponent implements OnInit {
       skills: [ 'Java', 'DynamoDB', 'AWS', 'Terraform', 'Terragrunt', 'TDD']
     },{
       title: 'Technical Lead',
+      url: 'http://vantek.io/',
       logo: 'https://www.wallstreetenglish.com.ar/hubfs/VANTEK_LOGO.png',
       employer: 'Vantek',
       to: new Date(2022,6,1),
@@ -26,6 +28,7 @@ export class ExperienceComponent implements OnInit {
       skills: ['Coaching', 'Team Leadership', 'Ionic', 'Java', 'Angular']
     },{
       title: 'FullStack Engineer',
+      url: 'http://vantek.io/',
       logo: 'https://www.wallstreetenglish.com.ar/hubfs/VANTEK_LOGO.png',
       employer: 'Vantek',
       to: new Date(2021,6,1),
@@ -34,8 +37,9 @@ export class ExperienceComponent implements OnInit {
       skills: ['Bash', 'DevOps', 'Ionic', 'Java', 'Spring Framework', 'Angular', 'PostgreSQL']
     },{
       title: 'FullStack Developer',
-      logo: 'https://contactcentersonline.com/wp-content/uploads/2021/01/Keepcon-Logos_Logotipo-1-e1609823381566.png',
       employer: 'Keepcon',
+      url: 'https://keepcon.com/',
+      logo: 'https://contactcentersonline.com/wp-content/uploads/2021/01/Keepcon-Logos_Logotipo-1-e1609823381566.png',
       to: new Date(2019,7,1),
       from: new Date(2017,11,1),
       description: '',
@@ -43,6 +47,7 @@ export class ExperienceComponent implements OnInit {
     },{
       title: 'Software Developer',
       employer: 'Codementor',
+      url: 'https://www.codementor.io/@gonzaloalvarez',
       logo: 'https://cdn.filestackcontent.com/cLv6OUoRMWoe3Vn3Pchg',
       to: new Date(2018,10,1),
       from: new Date(2018,6,1),
@@ -50,8 +55,9 @@ export class ExperienceComponent implements OnInit {
       skills: ['Algorithm Desing', 'Python', 'Java', 'SQL', 'Ruby']
     },{
       title: 'Software Developer',
-      logo: 'https://www.luminaamericas.com/wp-content/uploads/2022/02/Logo-Lumina_Mesa-de-trabajo-1-copia.png',
       employer: 'Lumina Americas',
+      url: 'https://www.luminaamericas.com/',
+      logo: 'https://www.luminaamericas.com/wp-content/uploads/2022/02/Logo-Lumina_Mesa-de-trabajo-1-copia.png',
       to: new Date(2017,11,1),
       from: new Date(2014,2,1),
       description: '',
@@ -70,5 +76,13 @@ export class ExperienceComponent implements OnInit {
 
   yearsDiffFromNow(from: Date) {
     return Math.floor((new Date().getTime() - from.getTime() ) /(1000 * 60 * 60 * 24 * 364));
+  }
+
+  monthsDiff(from: Date, to: Date) {
+    return Math.floor(((to.getTime() - from.getTime() ) /(1000 * 60 * 60 * 24 * 364) % 1) * 12)
+  }
+
+  monthsDiffFromNow(from: Date) {
+    return Math.floor(((new Date().getTime() - from.getTime() ) /(1000 * 60 * 60 * 24 * 364) % 1) * 12)
   }
 }
--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
+  projects = [
+    {
+      name: 'Ophrys',
+      logo: '../../assets/logos/ophrys.svg',
+      description: 'Cryptocurrency trading automation. Designed mainly to be adapted to any market. The main motivation was to explore Golang.',
+      technologies: ['Golang'],
+      url: 'https://github.com/gonzaloea/ophrys'
+    },
+    {
+      name: 'Ophrys GUI',
+      logo: '../../assets/logos/ophrys.svg',
+      description: 'Ophrys graphical user interface. The main motivation was to explore new versions of Vue.js.',
+      technologies: ['Vue.js'],
+      url: 'https://github.com/gonzaloea/ophrys-gui'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
